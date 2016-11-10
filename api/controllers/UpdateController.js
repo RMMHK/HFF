ebf373acd0e73ffc637dada5647bbce27a544f8d
RMMHK <comsatscs@yahoo.com>
2016-11-11 00:11:37 +0500

    ll

--- api/controllers/UpdateController.js
+++ api/controllers/UpdateController.js
@@ -9,16 +9,53 @@ module.exports = {
 
 
   send:function (req,res,next) {
+    var FCM = require('fcm-node');
 
+    var params= req.body;
+
+    var body= params.body;
+    var title = params.title;
+    var CK = params.keys;
+    var token = params.token;
+
+
+
+
+
+
+
+  },
+
+
+  send:function (req,res,next) {
+    var dish_names=[]
+    var container=[]
+    var dish_types=[]
+
+    var FCM = require('fcm-node');
+    var serverKey = 'AIzaSyAqx0agqYXjwKC5z1VjuS9ZneYIeAs63WU';
+    var fcm = new FCM(serverKey);
+    var token,dish_name,dish_types
+    var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+      to: token,
+      collapse_key: "1",
+
+      notification: {
+        title: "we have new dishes for you",
+        body: "tap to install"
+      },
+
+      data: {  //you can send only notification or only data(or include both)
+        dish_name: dish_name,
+        dish_list: container
+      }
+    };
     //take in put from dishname model// populate it and prepare an appropriate output..
     Dishname.find({}).populate('Types').then(function(data,err)
     {
       if (data)  // for preparing dishname file..
       {
-        var dish_names=[]
 
-        var container=[]
-        var dish_types=[]
         var data_length= data.length;
 
         for(i=0;i<data_length;i++)
@@ -43,7 +80,6 @@ module.exports = {
 
 
         }
-
         container.push({
 
           dishname:dish_name,dishType:dish_types
@@ -53,9 +89,32 @@ module.exports = {
 
       res.json({list:container})
       }
+      
+      Apptokens.find({}).then(function (data,err) {
 
+        if (data)
+        {
+           for(i=0;i<data.length;i++)
+           {
+             var app_token= data.application_token;
+
+             token=app_token //putting tokens on the line
+
+             fcm.send(message, function(err, response){
+               if (err) {
+                 console.log("Something has gone wrong!");
+               } else {
+                 console.log("Successfully sent with response: ", response);
+               }
+             });
+           }
+        }
+
+      })
 
     })
+
+
 
   }
 
