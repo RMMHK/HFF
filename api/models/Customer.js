3f4046cc69d776884555c258be49c1cef15b8897
RMMHK <comsatscs@yahoo.com>
2016-11-15 12:43:14 +0500

    ll

--- api/models/Customer.js
+++ api/models/Customer.js
@@ -8,7 +8,57 @@
 module.exports = {
 
   attributes: {
+    //CUSTOMER MODEL
+      name: {
+        type :'string',
+        required: true
 
+      },
+
+      cell: {
+        type :'string',
+        required: true,
+        unique: true
+      },
+
+      email: {
+        type:'string',
+        email: true,
+        required: true,
+        unique: true
+      },
+      cnic://ID card
+      {
+        type:'integer'
+      },
+
+      f_status://fraud status
+      {
+        type:'boolean'
+      },
+      f_warnings://fraud warnings
+
+      {
+        type:'integer'
+      },
+
+      current_location://location through GPS
+      {
+        type:'string'
+      },
+
+      v_id:
+      {
+        type:'string',
+        unique:true
+      },
+
+      token:
+      {
+        type:'string',
+        defaultsTo:'N/A'
+      },
+    
   }
 };
 
