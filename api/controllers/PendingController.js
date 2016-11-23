e86b8e3976b2715db50b4f894f61930cbff9192a
RMMHK <comsatscs@yahoo.com>
2016-11-23 14:48:05 +0500

    DD

--- api/controllers/PendingController.js
+++ api/controllers/PendingController.js
@@ -12,7 +12,38 @@ module.exports = {
   /**
    * `PendingController.order()`
    */
-  order: function (req, res) {
+  temp_order: function (req, res) {
+
+    var params = req.body;
+
+
+    var cus_lat= body.lat;
+    var cus_long= body.long;
+    var ordered_dish= body.ordered_dish;
+    var cus_token= body.cus_token;
+    var provider_id = body.ordered_provider;
+    var item_id = body.ordered_item_id;
+    var ordered_bill = body.ordered_price;
+    var ordered_quantity= body.ordered_quantity
+    var ordered_unit=body.ordered_unit
+
+    //check status of the shop and item
+    //make request to provider and wait
+
+    Pending.create({customer_token:cus_token,provider_id:provider_id,ordered_dish:ordered_dish,ordered_quantity:ordered_quantity,ordered_bill:ordered_bill,order_unit:ordered_unit}).then(function (tempOrder,err)
+    {
+
+    if(tempOrder)
+    {
+      //PUSH to provider
+
+      console.log(tempOrder);
+    }
+
+    })
+
+
+
 
 var geo_coder= require("geocoder");
 
--- api/models/Pending.js
+++ api/models/Pending.js
@@ -9,7 +9,62 @@ module.exports = {
 
   attributes: {
 
-    order : { type: 'string' }
+    customer_token: {
+      type: 'string'
+    },
+
+    provider_id: {
+      type: 'string'
+
+    },
+
+    customer_location: {
+      type: 'string',
+      defaultsTo:"null"
+
+    },
+
+    ordered_dish: {
+      type: 'string',
+      defaultsTo:"no dish"
+    },
+
+    ordered_quantity: {
+      type: 'string',
+      defaultsTo:"0"
+    },
+    ordered_bill: {
+      type: 'string',
+      defaultsTo: "0"
+    },
+
+    ordered_unit: {
+      type: 'string',
+      defaultsTo: "0"
+    },
+
+    provider_response: {
+      type: 'integer',
+      defaultsTo: -1 // means not responded  //0 for cancelled  //1 for accepted
+    },
+
+    guy_name: {
+      type: 'string',
+      defaultsTo: 'not assigned'
+    },
+
+    guy_cell: {
+      type: 'string',
+      defaultsTo: 'not available'
+    },
+
+
+    guy_status: {
+      type: 'string',
+      defaultsTo: 'not status yet'
+    },
+
   }
+
 };
 
