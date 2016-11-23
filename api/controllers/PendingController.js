e7a075ed944ee6330880766cf3ed573fe30c1c84
RMMHK <comsatscs@yahoo.com>
2016-11-23 16:19:37 +0500

    SS

--- api/controllers/PendingController.js
+++ api/controllers/PendingController.js
@@ -36,6 +36,7 @@ module.exports = {
 
 
         console.log(location.result)
+        var obj =location.result
     Pending.create({customer_token:cus_token,provider_id:provider_id,ordered_dish:ordered_dish,ordered_quantity:ordered_quantity,ordered_bill:ordered_bill,order_unit:ordered_unit}).then(function (tempOrder,err)
     {
 
