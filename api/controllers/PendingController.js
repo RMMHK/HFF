cc426fd31549ca8943a4de9e97e32ff90c5abe85
RMMHK <comsatscs@yahoo.com>
2016-11-25 16:26:56 +0500

    DD

--- api/controllers/PendingController.js
+++ api/controllers/PendingController.js
@@ -75,7 +75,7 @@ module.exports = {
                                   res.json({status:"Provider not responding"})
                               })
 
-                             },8000)
+                             },10000)
                             }
 
 
