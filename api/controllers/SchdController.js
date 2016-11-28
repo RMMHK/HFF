5b49f54e8f7eb99d92f6fa50493809bb13d51b4c
RMMHK <comsatscs@yahoo.com>
2016-11-29 00:12:51 +0500

    K

--- api/controllers/SchdController.js
+++ api/controllers/SchdController.js
@@ -22,8 +22,8 @@ module.exports = {
   starts:function (req) {
     var schedule = require('node-schedule');
 
-    var j = schedule.scheduleJob('5 * * * *', function(){
-      console.log('i will run after 5 sec');
+    var j = schedule.scheduleJob('*/1 * * * *', function(){
+      console.log('i will run after 1 min');
     });
 
   }
