f405418782f4ff1a78b2c674b64673c6682b203e
RMMHK <comsatscs@yahoo.com>
2016-11-28 23:33:03 +0500

    K

---
+++ api/controllers/SchdController.js
@@ -0,0 +1,22 @@
+/**
+ * SchdController
+ *
+ * @description :: Server-side logic for managing schds
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+
+  start: function (req,res) {
+ //   var schedule = require('node-schedule')
+    console.log('lala!');
+  //  let startTime = new Date(Date.now() + 5000);
+  //  let endTime = new Date(now.getTime() + 5000);
+  //  var j = schedule.scheduleJob({ start: startTime, end: endTime, rule: '*/1 * * * * *' }, function(){
+   //   console.log('Time for tea!');
+  //  });
+
+  }
+};
+
