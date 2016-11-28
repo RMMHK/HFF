4a0fa27e56638d39dddd1961c790345e0736660c
RMMHK <comsatscs@yahoo.com>
2016-11-28 23:38:47 +0500

    L"

--- api/controllers/SchdController.js
+++ api/controllers/SchdController.js
@@ -9,13 +9,13 @@ module.exports = {
 
 
   start: function (req,res) {
- //   var schedule = require('node-schedule')
-    console.log('lala!');
-  //  let startTime = new Date(Date.now() + 5000);
+    var schedule = require('node-schedule')
+ //   console.log('lala!');
+    let startTime = new Date(Date.now() + 5000);
   //  let endTime = new Date(now.getTime() + 5000);
-  //  var j = schedule.scheduleJob({ start: startTime, end: endTime, rule: '*/1 * * * * *' }, function(){
-   //   console.log('Time for tea!');
-  //  });
+    var j = schedule.scheduleJob({ start: startTime, rule: '*/1 * * * * *' }, function(){
+      console.log('Time for tea!');
+    });
 
   }
 };
