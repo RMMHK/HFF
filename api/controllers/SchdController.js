ad294c83659caf5b09081b532bbcfdb76ecc6b28
RMMHK <comsatscs@yahoo.com>
2016-11-28 23:56:17 +0500

    D

--- api/controllers/SchdController.js
+++ api/controllers/SchdController.js
@@ -11,7 +11,7 @@ module.exports = {
   start: function (req,res) {
     var schedule = require('node-schedule')
  //   console.log('lala!');
-    let startTime = new Date(Date.now() + 10000);
+    let startTime = new Date(Date.now() + 100000);
   //  let endTime = new Date(now.getTime() + 5000);
     var j = schedule.scheduleJob({ start: startTime, rule: '*/1 * * * * *' }, function(){
       console.log('EXECUTED!');
