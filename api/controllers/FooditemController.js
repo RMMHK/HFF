2573dc712d2d7a1ecbed5da233b3f7628eef845c
RMMHK <comsatscs@yahoo.com>
2016-12-05 17:33:11 +0500

    D

--- api/controllers/FooditemController.js
+++ api/controllers/FooditemController.js
@@ -356,7 +356,7 @@ function get_customer_location (lat,long,callback) {
       }
       address = sector+","+street;
       console.log(address)
-      return callback(address)
+      return callback(address+"")
     }
 
 
